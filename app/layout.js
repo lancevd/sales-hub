53bba262f1e94e6fc309539ff6a1d09ebbceed14
lancevd <olamosob@gmail.com>
2024-12-21 09:50:48 +0100

    display fixed

--- app/layout.js
+++ app/layout.js
@@ -10,10 +10,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="h-screen flex flex-col">
         <Header />
-        <Sidebar />
-        {children}
+        <div className="flex flex-1">
+          <Sidebar />
+          <main className="flex-1 bg-gray-200 p-4 overflow-auto">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -3,9 +3,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <main className="flex-1 bg-gray-200 p-4 overflow-auto">
-        <p className="text-lg font-semibold">Dashboard</p>
-      </main>
+        <p className="text-lg font-semibold">Display</p>
     </div>
   );
 }
